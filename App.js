1ffb428a183d87982b3762d6d266f8562c0a749f
Andrei Chornii <andrei.chornii@gmail.com>
2021-08-07 23:06:13 +0300

    form

---
+++ App.js
@@ -0,0 +1,192 @@
+<script src="http://localhost:8097"></script>
+
+
+
+import React from "react";
+import { useCallback, useRef, useState } from "react";
+import { Linking, Alert, Button, Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import {useForm, Controller} from "react-hook-form";
+
+// const TextInANest = () => {
+const titleText = "JET CARDS Service - Sign in";
+// };
+
+export default () => {
+  const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
+    defaultValues: {
+      login: '',
+      password: ''
+    }
+  });
+
+  const onPress = data => {
+    console.log(data);
+};
+
+  const onSubmit = data => {
+      console.log(data);
+  };
+
+  const onChange = arg => {
+    return {
+      value: arg.nativeEvent.text,
+    };
+  };
+
+  if ( (typeof errors.login !== 'undefined') && (typeof errors.password !== 'undefined') ) {
+    console.log('login & password');
+  } else if(typeof errors.login !== 'undefined') {
+    console.log('login');
+  } else if(typeof errors.password !== 'undefined') {
+    console.log('password');
+  }
+  console.log('errors', errors);
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.middle}>
+        <View style={styles.header}>
+          <Text style={styles.titleText}>
+            {titleText}
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.contentelement}>
+            <Image
+              style={styles.image}
+
+              source={require("./img/smile.png")}
+            />
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={value => onChange(value)}
+                  value={value}
+                  placeholder="Login"
+                />
+              )}
+              name="login"
+              rules={{ required: true }}
+            />
+          </View>
+          <View style={styles.contentelement}>
+            <Image
+              style={styles.image}
+              source={require("./img/lock.png")}
+            />
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={value => onChange(value)}
+                  placeholder="Password"
+                  value={value}
+                  secureTextEntry={true}
+                />
+              )}
+              name="password"
+              rules={{ required: true }}
+            />
+          </View>
+          <View style={styles.contentelement}>
+            <Button
+              title="SIGN IN"
+              color="lightgrey"
+              onPress={handleSubmit(onSubmit)}
+            />
+          </View>
+          <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+            <Text style={styles.appButtonText}>{"Forgot your Login or Password?"}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 10,
+  },
+  middle: {
+    flex: 0.5,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 10,
+    alignItems: "center",
+
+  },
+  header: {
+    flex: 0.25,
+    backgroundColor: "#de2768",
+    borderRadius: 5,
+    position: "relative",
+    width: "75%",
+    justifyContent: 'center',
+    marginTop: -40
+  },
+  content: {
+    flex: 0.75,
+    // backgroundColor: "green",
+    justifyContent: "space-around",
+    width: "90%"
+    // alignItems: "center"
+  },
+  titleText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff"
+  },
+  contentelement: {
+    borderWidth: 1,
+    borderColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  input: {
+    height: 35,
+    width: 60,
+    margin: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgrey",
+    padding: 10,
+    flex: 0.75,
+  },
+  image: {
+    resizeMode: "cover",
+    height: 25,
+    width: 25,
+    marginRight: 25,
+    // borderWidth: 1,
+    // borderColor: 'brown',
+    alignSelf: "center"
+  },
+  appButtonContainer: {
+    // elevation: 8,
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  appButtonText: {
+    fontSize: 12,
+    color: "#de2768",
+    // fontWeight: "bold",
+    alignSelf: "center",
+    // textTransform: "uppercase"
+  }
+});
